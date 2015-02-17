18b2600dc24669773c62257be3f3811a661b17f5
Adam Campbell <awjc@iastate.edu>
2015-02-17 02:45:14 -0600

    Add test images

--- php/phptest.php
+++ php/phptest.php
@@ -5,8 +5,15 @@
         <title>PHP test</title>
     </head>
     <body>
+        Hola
         <?php
-            echo 'php is working'
+            try {
+                $db = new PDO('mysql:host=localhost;dbname=SE329Proj2;charset=utf8', 'root', 'potato44');
+                echo $db
+            } catch (PDOException $ex) {
+                echo "An Error occured!"; //User friendly message/message you want to show to user
+                echo $ex->getMessage();
+            }
         ?>
     </body>
 </html>
