1981b958d7f17b9200d0bfb0dfb2488c8beb4be4
Adam Campbell <awjc@iastate.edu>
2015-02-17 01:13:04 -0600

    initial commit - setup HTML/JS/CSS for client website

---
+++ FacialRecognition.js
@@ -0,0 +1,32 @@
+/// <reference path="js/jquery-2.1.0.js" />
+/// <reference path="js/jquery-2.1.0-vsdoc.js" />
+
+$(function () {
+    function doTraining() {
+
+    }
+
+    function retrieveMatch(img) {
+
+    }
+
+    //button = $('<input type="button" value="new button"/>');
+    //$('body').append(button);
+    //button.click(function () { $('#content').text('my content') });
+    //for (i = 0; i < 10; i++) {
+    //    button = $('<input type="button" value="new button ' + i + '"/>');
+    //    $('body').append(button);
+    //    button.click((function (x) {
+    //        return function () {
+    //            $('#content').text(x);
+    //        }
+    //    })(i));
+    //}
+
+    $('#trainBtn').click(function() {
+        $('#content').append('hihgiuhihihihihihhihihihihihihihihiye');
+    });
+    $('#recognizeBtn').click(function() {
+        $('#content').text('bye');
+    });
+});
