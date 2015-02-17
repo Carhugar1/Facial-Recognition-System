ce12296cffe95083db8bafa3c05b239fafb2dfd7
Adam Campbell <awjc@iastate.edu>
2015-02-17 03:13:04 -0600

    Initial PHP DB workings

--- php/phptest.php
+++ php/phptest.php
@@ -3,17 +3,24 @@
     <head>
         <meta charset="utf-8" />
         <title>PHP test</title>
+        <link rel="stylesheet" href="../css/mainpage.css">
     </head>
     <body>
         Hola
-        <?php
-            try {
-                $db = new PDO('mysql:host=localhost;dbname=SE329Proj2;charset=utf8', 'root', 'potato44');
-                echo $db
-            } catch (PDOException $ex) {
-                echo "An Error occured!"; //User friendly message/message you want to show to user
-                echo $ex->getMessage();
-            }
-        ?>
+        <br />
+        <div class="content">
+            <?php
+                try {
+                    $db = new PDO('mysql:host=localhost;dbname=SE329Proj2;charset=utf8', 'root', 'potato44');
+                    $stmt = $db->query("SELECT * FROM faces");
+                    $arr = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    print_r($arr);
+                } catch (PDOException $ex) {
+                    echo "An Error occured!"; //User friendly message/message you want to show to user
+                    echo $ex->getMessage();
+                }
+            ?>
+        </div>
+        Adios
     </body>
 </html>
